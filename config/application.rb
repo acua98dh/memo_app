982f0c2bccf469b7dd031bb8bf8af4cf3183975a
acua98dh <vbnsibc@yahoo.co.jp>
2018-07-13 16:36:35 +0000

    Add:StaticPages

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module MemoApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    #Use Bootstrap
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
