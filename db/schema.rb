48c6aee2f519b08964add6cb743c62f1168639f9
acua98dh <vbnsibc@yahoo.co.jp>
2018-07-14 02:58:55 +0000

    Create model Tags using scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_022818) do
+ActiveRecord::Schema.define(version: 2018_07_14_024735) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.string "review"
     t.string "freq"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
