ef3b27fe25bdc4bd9ee2b67ffc311e8b92a69543
acua98dh <vbnsibc@yahoo.co.jp>
2018-07-20 01:47:21 +0000

    Update:reviews in views

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
     has_many :review_tags, foreign_key: "review_id", dependent: :destroy
+    validates :name, presence: true
+    validates :rating, format: { with: /[1-5]/,message: "値が不正です" }
 end
