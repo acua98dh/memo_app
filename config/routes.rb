7da1f5a2e9ddf949ef539f1ba0716d4c9c2c603e
acua98dh <vbnsibc@yahoo.co.jp>
2018-07-20 13:01:35 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'users/show'
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+  resources :users, only: [:show]
   resources :tags
   resources :reviews
-  devise_for :users
   root 'static_pages#home'
 #  match '/help',    to: 'static_pages#help',    via: 'get'
 
